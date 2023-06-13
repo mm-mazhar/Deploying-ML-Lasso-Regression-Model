6cbc467957b99fedcebffd87270ddaa6a832d924
Mazhar <mazqoty.01@gmail.com>
2023-06-13 17:48:16 +0300

    update publish_model.sh

--- 4.ci_and_publishing/model-package/publish_model.sh
+++ 4.ci_and_publishing/model-package/publish_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Building packages and uploading them to a Gemfury repository
-
+echo "Building packages and uploading them to a Gemfury repository"
 GEMFURY_URL=$GEMFURY_PUSH_URL
 echo $GEMFURY_URL
 set -e
