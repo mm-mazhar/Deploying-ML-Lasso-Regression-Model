26b20011ff251b611befc0e0d8abfc04c4212b5b
Mazhar <mazqoty.01@gmail.com>
2023-06-14 10:42:01 +0300

    update

--- 4.ci_and_publishing/model-package/publish_model.sh
+++ 4.ci_and_publishing/model-package/publish_model.sh
@@ -10,6 +10,13 @@ DIRS="$@"
 BASE_DIR=$(pwd)
 SETUP="setup.py"
 
+echo "-----------"
+echo "DIRS: $DIRS"
+echo "BASE_DIR: $BASE_DIR"
+echo "SETUP: $SETUP"
+echo "GEMFURY_URL: $GEMFURY_URL"
+echo "-----------"
+
 warn() {
     echo "warn"
     echo "$@" 1>&2
@@ -24,11 +31,15 @@ die() {
 echo "Building packages and uploading them to GEMFURY_URL"
 build() {
     DIR="${1/%\//}"
+    echo "------------"
     echo "Checking directory $DIR"
+    echo "------------"
     cd "$BASE_DIR/$DIR"
     [ ! -e $SETUP ] && warn "No $SETUP file, skipping" && return
     PACKAGE_NAME=$(python $SETUP --fullname)
+    echo "------------"
     echo "Package $PACKAGE_NAME"
+    echo "------------"
     python "$SETUP" sdist bdist_wheel || die "Building package $PACKAGE_NAME failed"
     for X in $(ls dist)
     do
